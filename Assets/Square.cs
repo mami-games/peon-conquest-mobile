e101b298cf8291c8bfe6d628d29a840fadb6f90f
Michaël <michael@folkshr.com>
2024-05-26 20:52:03 -0400

    Fichiers manquants

--- Assets/Square.cs
+++ Assets/Square.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Square : MonoBehaviour
 {
-    private float Speed = 5f;
+    private float Speed = 20f;
 
     // Start is called before the first frame update
     void Start()
